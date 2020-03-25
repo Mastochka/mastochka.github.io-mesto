61f6da5eb426fe14fe67b47306c70ba5ccb69b6e
Mikhail Safonov <se1dom@yandex.ru>
2020-03-25 03:38:19 +0300

    alfa version of mesto project with webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackMd5Hash = require('webpack-md5-hash');
+const ImageWebpackLoader = require('image-webpack-loader');
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|ico|svg)$/,
+                use: [
+                        'file-loader?name=../dist/images/[name].[ext]',
+                        {
+                                loader: 'image-webpack-loader',
+                                options: {}
+                        },
+                ]
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=./vendor/fonts/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}),
+        new HtmlWebpackPlugin({
+            // Означает, что:
+            inject: false, // стили НЕ нужно прописывать внутри тегов
+            
+            template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+            filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
+          }),
+
+    ]
+};
